bae03598c529d0a447b5cec49767466557003b5e
JohnnyElvis <jayjay89@gmail.com>
2022-02-23 16:22:02 +0100

    Update cockpit.sh

--- cockpit.sh
+++ cockpit.sh
@@ -5,5 +5,5 @@ sudo apt update && sudo apt install cockpit -y
 
 #Make Firewall in Network plugin work
 sudo apt-get remove ufw -y 
-sudo apt get update && sudo apt-get install firewalld -y
+sudo apt-get update && sudo apt-get install firewalld -y
 sudo firewall-cmd --add-service=ssh --permanent && sudo firewall-cmd --add-service=cockpit --permanent && sudo systemctl enable firewalld && sudo systemctl start firewalld
