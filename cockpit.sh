5a3c0a52327a7930fa57a1e653eaef84f022781b
JohnnyElvis <jayjay89@gmail.com>
2022-02-23 16:35:30 +0100

    Update cockpit.sh

--- cockpit.sh
+++ cockpit.sh
@@ -7,3 +7,6 @@ sudo apt update && sudo apt install cockpit -y && sudo systemctl start cockpit
 sudo apt-get remove ufw -y 
 sudo apt-get update && sudo apt-get install firewalld -y
 sudo firewall-cmd --add-service=ssh --permanent && sudo firewall-cmd --add-service=cockpit --permanent && sudo systemctl enable firewalld && sudo systemctl start firewalld
+
+#Fix LVM not being displayed
+sudo apt-get update && sudo apt-get install udisks2-lvm2 -y
